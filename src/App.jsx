06acfea4779222845990773551fec2ec82cc8c2f
Axel Ahlander <axel.ahlander@gmail.com>
2025-02-05 14:13:52 +0100

    ext 2

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,18 @@ function App() {
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
   const [currentEmail, setCurrentEmail] = useState(null);
+  const [searchText, setSearchText] = useState('');
 
   const unreadEmails = emails.filter(email => !email.read)
   const starredEmails = emails.filter(email => email.starred)
 
+  const handleSearchText = (event) => {
+    setSearchText(event.target.value.toLowerCase());
+  }
+
+  let searchedEmails = emails.filter(email => email.title.toLowerCase().includes(searchText.toLowerCase()));
+
+  
   const handleClick = (email) => {
     setCurrentEmail(email);
   }
@@ -58,6 +66,62 @@ function App() {
     filteredEmails = getStarredEmails(filteredEmails)
 
   if (currentEmail == null){
+    if (searchedEmails.length > 0){
+      return (
+        <>
+        <div className="app">
+          <header className="header">
+            <div className="left-menu">
+              <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
+                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+              </svg>
+    
+              <img
+                src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+                alt="gmail logo"
+              />
+            </div>
+    
+            <div className="search">
+              <input className="search-bar" placeholder="Search mail" onChange = {handleSearchText}/>
+            </div>
+          </header>
+          <nav className="left-menu">
+            <ul className="inbox-list">
+              <li
+                className={`item ${currentTab === 'inbox' ? 'active' : ''}`}
+                onClick={() => setCurrentTab('inbox')}
+              >
+                <span className="label">Inbox</span>
+                <span className="count">{unreadEmails.length}</span>
+              </li>
+              <li
+                className={`item ${currentTab === 'starred' ? 'active' : ''}`}
+                onClick={() => setCurrentTab('starred')}
+              >
+                <span className="label">Starred</span>
+                <span className="count">{starredEmails.length}</span>
+              </li>
+    
+              <li className="item toggle">
+                <label htmlFor="hide-read">Hide read</label>
+                <input
+                  id="hide-read"
+                  type="checkbox"
+                  checked={hideRead}
+                  onChange={e => setHideRead(e.target.checked)}
+                />
+              </li>
+            </ul>
+          </nav>
+          <Emailscomponent filteredEmails = {searchedEmails} toggleRead = {toggleRead} toggleStar = {toggleStar} onClick = {handleClick}/>
+        </div>
+        </>
+      )
+    }
+    else {
+      
+    
     return (
       <>
       <div className="app">
@@ -110,7 +174,7 @@ function App() {
       </>
     )
   }
-
+  }
   else {
     return (
       <>
@@ -118,7 +182,6 @@ function App() {
       </>
     )
   }
-  
     
 }
 
